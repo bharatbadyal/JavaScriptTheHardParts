c8d01def7139750b2d274e814afde09f189844e8
Bharat <myprof219@gmail.com>
2025-02-01 20:29:36 +0530

    Generalized Functions

---
+++ FunctionsAndCallBacks/file2.js
@@ -0,0 +1,18 @@
+function tenSquared() {
+  return 10 * 10;
+}
+
+console.log(tenSquared());
+
+function numSquare(num) {
+  return num * num;
+}
+
+console.log(numSquare(15));
+
+// GENERALIZING FUNCTION
+// "Parameters"(placeholders) meaen we don't need to decide what data to run our functionality on until we run the function
+// --- Then provide an actual value ('argument') when we run the function
+
+// Higher order function follow this same principle.
+// -- We may not want too decide exactly what some of our functionalty is until we run our function.
