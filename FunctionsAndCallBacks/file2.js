f254c66a1960cb7852b097522e773e535fe8c9a3
Bharat <myprof219@gmail.com>
2025-02-05 21:34:02 +0530

    BubbleSort

--- FunctionsAndCallBacks/file2.js
+++ FunctionsAndCallBacks/file2.js
@@ -72,3 +72,20 @@ switch (sign) {
     console.log("Sum of Random Number: ", randomNum + randomNum);
     break;
 }
+
+function bubbleSort(arr) {
+  let n = arr.length;
+  for (let i = 0; i < n - 1; i++) {
+    for (let j = 0; j < n - i - 1; j++) {
+      if (arr[j] > arr[j + 1]) {
+        // Swap arr[j] and arr[j+1]
+        [arr[j], arr[j + 1]] = [arr[j + 1], arr[j]];
+      }
+    }
+  }
+  return arr;
+}
+
+// Example usage:
+let numbers = [64, 34, 25, 12, 22, 11, 90];
+console.log("Sorted array:", bubbleSort(numbers));
