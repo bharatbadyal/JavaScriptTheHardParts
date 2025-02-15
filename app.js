552cc9bacd24e0efc63f3c9598e4d55e1294caee
Bharat <myprof219@gmail.com>
2025-02-15 21:53:35 +0530

    ---

--- app.js
+++ app.js
@@ -6,3 +6,18 @@ document
     console.log(this);
   });
 //using arrow function it will give the global context so always use named function or anonymos function
+import express from "express";
+const app = express();
+const port = 3000;
+
+app.get("/", (req, res) => {
+  res.send("<h1>Welcome to the Home Page</h1>");
+});
+
+app.get("/twiter", (req, res) => {
+  res.send("<h1>Welcome to the Twitter Page</h1>");
+});
+
+app.listen(port, () => {
+  console.log(`Listning to port no:${port}`);
+});
